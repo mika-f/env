ce4dc2f5826b0c0225cc228c3a7cbd86b224ec33
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-02 08:42:12 +0900

    chore: add extensions

--- Visual Studio Code/install.sh
+++ Visual Studio Code/install.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-code --install-extension Gimly81.matlab
-code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension auchenberg.vscode-browser-preview
 code --install-extension be5invis.toml
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension eg2.tslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension fallenwood.vimL
+code --install-extension Gimly81.matlab
 code --install-extension goessner.mdmath
 code --install-extension henriiik.vscode-perl
 code --install-extension maelvalais.autoconf
 code --install-extension mhmadhamster.postcss-language
+code --install-extension mikestead.dotenv
+code --install-extension MS-CEINTL.vscode-language-pack-ja
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.Go
 code --install-extension octref.vetur
-code --install-extension robertohuertasm.vscode-icons
+code --install-extension PeterJausovec.vscode-docker
 code --install-extension shinnn.stylelint
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vscode-icons-team.vscode-icons
 code --install-extension yzhang.markdown-all-in-one
