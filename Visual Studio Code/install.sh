cfbc88092a22731416f8104ac5c2674c2ecbe796
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-02 09:06:38 +0900

    feat(vscode): add extensions

--- Visual Studio Code/install.sh
+++ Visual Studio Code/install.sh
@@ -1,18 +1,25 @@
-#!/bin/sh
 code --install-extension Gimly81.matlab
-code --install-extension MS-CEINTL.vscode-language-pack-ja
 code --install-extension be5invis.toml
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eg2.tslint
+code --install-extension esbenp.prettier-vscode
 code --install-extension goessner.mdmath
 code --install-extension henriiik.vscode-perl
 code --install-extension maelvalais.autoconf
+code --install-extension mathiasfrohlich.Kotlin
 code --install-extension mhmadhamster.postcss-language
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.csharp
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
+code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension octref.vetur
+code --install-extension PeterJausovec.vscode-docker
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension shinnn.stylelint
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension toba.vsfire
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vncz.vscode-apielements
 code --install-extension yzhang.markdown-all-in-one
